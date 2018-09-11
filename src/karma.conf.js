67b0591cd1a1a410ee69e8e5b1228b50fdca266b
Maxime <mpcaudan@gmail.com>
2018-09-12 00:10:54 +0200

    Updated config for running test with ng test & ng e2e

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -26,6 +26,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLauchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', 'disable-gpu']
+      }
+    },
     singleRun: false
   });
 };
